fb0b6af9526212bdf26d0be17b644b3c672578c1
Akhilesh26 <akhileshpimple3@gmail.com>
2025-06-24 18:58:05 +0530

    create function for connection with postgreSQL

---
+++ Backend/ConnectDB.js
@@ -0,0 +1,22 @@
+const { Client } = require("pg");
+
+const connectToPostgresSQL = () => {
+    const client = new Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'bot-partner',
+        password: 'postgreSqlispassword@26',
+        port: 5432,
+    });
+
+    client.connect()
+        .then(() => {
+            console.log("Connected to PostgreSQL");
+        })
+        .catch((err) => {
+            console.error("PostgreSQL connection error", err);
+        });
+};
+
+
+module.exports = { connectToPostgresSQL };
