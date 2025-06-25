8ec63513fb85af5da575880d3a62b56c3cff867a
Akhilesh26 <akhileshpimple3@gmail.com>
2025-06-25 16:58:59 +0530

    add status codes and handle data correctly

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -5,10 +5,13 @@ const client = connectDB.connectToPostgresSQL();
 async function createUser(username, email, password, firstname, lastname, phonenumber, dateofbirth) {
     try {
         const response = await client.query(`INSERT INTO users(username, email, password, firstname, lastname, phonenumber, dateofbirth) VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING *`,[username, email, password, firstname, lastname, phonenumber, dateofbirth]);
-        return {status : 201, messege: "user registered", data: response};
+        return {status : 201, message: "user registered", data: response.rows[0]};
     } catch (error) {
-        return {status: 400, messege : "User register error occured"};
+        if(error.code == 23505) return{status: 409, message: "username or email is already present"};
+        return {status: 400, message : "User register error occured"};
     }
 };
+
+
 
 module.exports = {createUser};
