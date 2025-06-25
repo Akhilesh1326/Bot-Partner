3cede78c0839ff9f6f8632acdea1226bfbc2fc63
Akhilesh26 <akhileshpimple3@gmail.com>
2025-06-25 15:16:28 +0530

    update for return the data as well

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,11 +1,11 @@
-const connectToPostgresSQL = require('../ConnectDB');
+const connectDB = require('../ConnectDB');
 
-const client = connectToPostgresSQL();
+const client = connectDB.connectToPostgresSQL();
 
 async function createUser(username, email, password, firstname, lastname, phonenumber, dateofbirth) {
     try {
         const response = await client.query(`INSERT INTO users(username, email, password, firstname, lastname, phonenumber, dateofbirth) VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING *`,[username, email, password, firstname, lastname, phonenumber, dateofbirth]);
-        return {status : 201, messege: "user registered"};
+        return {status : 201, messege: "user registered", data: response};
     } catch (error) {
         return {status: 400, messege : "User register error occured"};
     }
