4545d1657abc1e83aef395f81142662f588396b9
Akhilesh26 <akhileshpimple3@gmail.com>
2025-06-25 15:17:50 +0530

    update register api call for jwt token and add cors policies and add middelwares for json parsing

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,30 @@
 const express = require("express")
-const app = express()
 const PORT = process.env.PORT || 8000;
 const ConnectDB = require("./ConnectDB");
+const cors = require('cors');
+const bodyParser = require('body-parser')
+require('dotenv').config();
 
+// JWT related imports
+const cookieParser = require('cookie-parser');
+const http = require('http');
+const jwt = require('jsonwebtoken');
+
+// Server by express
+const app = express()
+
+// cors policy
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE'],
+    credentials: true
+}));
+
+// Middlewares
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 
 // Imports for controllers 
@@ -26,15 +48,24 @@ app.post('/register', async(req, res)=>{
     if(!phonenumber) res.status(422).json({message:"phonenumber is missing"});
     if(!dateofbirth) res.status(422).json({message:"dateofbirht is missing"});
     try {
-        const res = await createUser(username, email, password, firstName, lastname, phonenumber, dateofbirth);
-        if(res.status == 201){
+        const DB_res = await createUser(username, email, password, firstName, lastname, phonenumber, dateofbirth);
+
+        if(DB_res.status == 201){
+            const token = jwt.sign({
+                userId: DB_res.data.userid,
+                username: DB_res.data.username
+            }, process.env.JWT_SECRET);
+            res.cookie('userCookie', token);
             res.status(200).json({message:"registration successful"});
         }
         else{
             res.status(500).json({message:"registration unsuccessful"});
         }
     } catch (error) {
+        console.log("Error while registering user", error.message);
         res.status(500);
     }
 })
+
+
 app.listen(PORT, ()=>console.log("Server started at ", PORT));
