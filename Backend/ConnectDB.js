6fe6d5c43ff0e4d6a9bc9032367fa5f8e2af1ad6
Akhilesh26 <akhileshpimple3@gmail.com>
2025-06-25 15:15:52 +0530

    nothing

--- Backend/ConnectDB.js
+++ Backend/ConnectDB.js
@@ -1,24 +1,28 @@
+
 const { Client } = require("pg");
 
-const connectToPostgresSQL = () => {
-    const client = new Client({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'bot-partner',
-        password: 'postgreSqlispassword@26',
-        port: 5432,
-    });
+let client;
 
-    client.connect()
-        .then(() => {
-            console.log("Connected to PostgreSQL");
-        })
-        .catch((err) => {
-            console.error("PostgreSQL connection error", err);
+const connectToPostgresSQL = () => {
+    if (!client) {
+        client = new Client({
+            user: 'postgres',
+            host: 'localhost',
+            database: process.env.PG_DB_NAME,
+            password: process.env.PG_PASSWORD,
+            port: process.env.PG_PORT,
         });
+
+        client.connect()
+            .then(() => {
+                console.log("Connected to PostgreSQL");
+            })
+            .catch((err) => {
+                console.error("PostgreSQL connection error", err);
+            });
+    }
 
     return client;
 };
-
 
 module.exports = { connectToPostgresSQL };
