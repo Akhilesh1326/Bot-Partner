ac294a72b74ce083862cbef1f0cb7b0ad020a834
Akhilesh26 <akhileshpimple3@gmail.com>
2025-07-02 20:00:43 +0530

    create connection to MongoDB

--- Backend/ConnectDB.js
+++ Backend/ConnectDB.js
@@ -27,12 +27,15 @@ const connectToPostgresSQL = () => {
 };
 
 // MongoDb connection
-mongoose.connect(" mongodb://localhost:27017/bot-partner" , {
-    useNewUrlParser : true ,
-    useUnifiedTopology : true ,
-})
-.then(() => console.log("MongoConnect"))
-.catch((err) => console.error("MongoDB connection Error" , err) );
+
+const connectToMongo = () =>{
+    mongoose.connect("mongodb://localhost:27017/bot-partner" , {
+        useNewUrlParser : true ,
+        useUnifiedTopology : true ,
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection Error" , err) );
+}
 
 
-module.exports = { connectToPostgresSQL };
+module.exports = { connectToPostgresSQL, connectToMongo };
